f086a7a3dba8208284442d06f75ada7f8d197fb4
truebigsand <3244346642@qq.com>
2023-12-16 20:04:21 +0800

    Add: to Chapter 3.2 Add: answer of P3.1-1 Add: example of numeric limits of floats

---
+++ code/answers/P3.1-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I love playing" << std::endl;
+    std::cout << "Genshin" << std::endl;
+    std::cout << "Impact" << std::endl;
+    return 0;
+}
---
+++ code/numeric-limits-of-floats.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+template <typename T>
+auto _max = std::numeric_limits<T>::max();
+template <typename T>
+auto _min = std::numeric_limits<T>::min();
+#define out(x) std::cout << #x << ": " << (x) << std::endl;
+#define out_max(t) out(_max<t>);
+#define out_min(t) out(_min<t>);
+int main() {
+	
+	out_max(float)
+	out_min(float)
+	
+	out_max(double)
+	out_min(double)
+	
+	out_max(long double)
+	out_min(long double)
+	
+	return 0;
+}
